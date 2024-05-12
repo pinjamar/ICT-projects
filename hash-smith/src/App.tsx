daa255385f6e21db3a1fd4e0e162bb8cb52ed34c
Ivan <pinjamar@gmail.com>
2024-05-12 16:05:56 +0200

    cointainers built

--- hash-smith/src/App.tsx
+++ hash-smith/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import SmallContainer from './components/SmallContainer';
+import BigContainer from './components/BigContainer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="small-containers-group">
+        <SmallContainer />
+        <SmallContainer />
+        <SmallContainer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="big-containers-group-one">
+        <BigContainer />
+        <BigContainer />
+        <BigContainer />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="big-containers-group-two">
+        <BigContainer />
+        <BigContainer />
+        <BigContainer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ hash-smith/src/components/SmallContainer.tsx
@@ -0,0 +1,27 @@
+import './containers.css';
+
+const SmallContainer = () => {
+  return (
+    <div className="small-container">
+      <div className="small-first-row">
+        <div className="miner small-miner-1"></div>
+        <div className="miner small-miner-2"></div>
+        <div className="miner small-miner-3"></div>
+        <div className="miner small-miner-4"></div>
+      </div>
+      <div className="small-second-row">
+        <div className="miner small-miner-5"></div>
+        <div className="miner small-miner-6"></div>
+        <div className="miner small-miner-7"></div>
+        <div className="miner small-miner-8"></div>
+        <div className="miner small-miner-9"></div>
+        <div className="miner small-miner-10"></div>
+        <div className="miner small-miner-11"></div>
+        <div className="miner small-miner-12"></div>
+        <div className="miner small-miner-13"></div>
+      </div>
+    </div>
+  );
+};
+
+export default SmallContainer;
